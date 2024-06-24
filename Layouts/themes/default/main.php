afedbacd94d835dced98a21bac7739f032077764
Rodrigo <rodrigochantel@gmail.com>
2024-06-24 08:22:58 -0300

    create new branch

--- Layouts/themes/default/main.php
+++ Layouts/themes/default/main.php
@@ -1,6 +1,6 @@
 <?php
     $main = "
-        <main id='main'>
+        <main>
             <div>
                 <div class='container'>
                     <div class='row text-white py-5'>
@@ -9,7 +9,7 @@
                                 <p class='fs-1 p-0 m-0'>I'm Rodrigo Chantel</p>
                                 <h1>Back-end Developer</h1>
                                 <p class='w-75 d-flex justify-content-between'>
-                                    <span class='ps-1'><span class='text-danger'>2</span> projects</span>
+                                    <span class='ps-1'><span class='text-danger'>0</span> projects</span>
                                     <span class='ps-1'><span class='text-danger'>27</span> y/o</span>
                                     <span class='ps-1'><span class='text-danger'>He/His</span> pronouns</span>
                                 </p>
@@ -18,7 +18,7 @@
                             </div>
                         </div>
                         <div class='col-md-6 text-center d-flex justify-content-center align-items-center'>
-                            <img class='profile' src='Assets/images/Rodrigo-profile-ex.webp' alt=''>
+                            <img height='350' src='Assets/images/Rodrigo-profile-ex.webp' alt=''>
                         </div>
                     </div>
     ";
--- about.php
+++ about.php
@@ -15,32 +15,9 @@
             </ul>
         </div>
     </div>
-   <div class="row">
-        <div class="col-md-12 text-white">
-            <p>
-                Tenho 27 anos e sou desenvolvedor back end. Além de meu foco principal, também possuo habilidades em desenvolvimento 
-                front end. Tenho conhecimentos básicos em MySQL e SQL Server, e as tecnologias que utilizo incluem HTML, CSS, e 
-                JavaScript, frequentemente com os frameworks Bootstrap e Laravel. Em breve, pretendo me tornar um programador full 
-                stack, iniciando meu aprendizado com Vue.
-            </p>
-            <p>
-                Entre meus projetos pessoais que comprovam minha experiência com desenvolvimento, destaco o site e sistema da Kristta. 
-                O que começou como um projeto de portfólio transformou-se em minha fonte de renda extra. Juntamente com dois sócios, 
-                Wilkerson Berg e Weslei Santos, formamos a agência de marketing Kristta. Através do site www.kristta.com.br, oferecemos 
-                serviços de criação de sites, pequenos sistemas, criação de identidade visual, estratégia de marketing geral e tráfego 
-                pago, embora este último não seja nosso foco principal.
-            </p>
-            <p>
-                Estou cursando Ciências da Computação, com previsão de término em agosto de 2025, e pretendo fazer uma pós-graduação 
-                logo após concluir a graduação.
-            </p>
-            <p>
-                Sou pai de um garoto chamado André, atualmente com dois anos e meio de idade, que é minha maior fonte de alegria, 
-                junto com minha esposa.
-            </p>
-        </div>
-   </div>
-
+    <div class='row'>
+        
+    </div>
 
 <?php
         echo $endMain; //Finaliza o main
